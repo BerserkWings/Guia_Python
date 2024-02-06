46179718f428f850c3e6251d708c96a3be01b6d7
Vyanei Salazar <manchas_0207@hotmail.com>
2024-02-06 03:06:38 -0600

    Actualizando Variables_Tipos_Datos.py

--- Conceptos Básicos/Variables_Tipos_Datos.py
+++ Conceptos Básicos/Variables_Tipos_Datos.py
@@ -1,42 +1,47 @@
 """
-Las variables en Python son como nombres que se le asignan a los datos que manejamos. Piensa en una variable como un
-nombre que pones a un valor, para poder referirte a él y utilizarlo en diferentes partes de tu código.
-
-Variables
-Una variable en Python es como un nombre que se le asigna a un dato. No es necesario declarar el tipo de dato, ya que
-Python puede inferirlo.
-
-Cadenas (Strings)
-Las cadenas son secuencias de caracteres que se utilizan para manejar texto. Son inmutables, lo que significa que una
-vez creadas, no puedes cambiar sus caracteres individuales.
-
-Números
-Python maneja varios tipos numéricos, pero nos centramos principalmente en:
-- Enteros (Integers): Números sin parte decimal.
-- Flotantes (Floats): Números que incluyen decimales.
+Estos son algunos ejemplos sobre variables y tipos de datos que se pueden utilizar en Python.
 """
 
+print("\n-----------------------------------------------------------------------------------------------------------\n")
 # Variable tipo String
+print("\t Variable Tipo String \t\n")
 cadena = "Esta es una cadena/string"
 
-print(type(cadena))
+print("La variable cadena es del tipo: " + str(type(cadena)))
+
 print(cadena)
 
-print()  # Este es un espacio namas
+print("\n-----------------------------------------------------------------------------------------------------------\n")
 
-# Variable tipo Int (Entero) y aplicando type casting
+# Variable tipo Integer (Entero) y aplicando type casting
+print("\t Variable Tipo Integer \t\n")
 puertos = 65535
 
-print(type(puertos))
+print("La variable puertos es del tipo: " + str(type(puertos)))
+
 print("Actualmente existen estos puertos: " + str(puertos))  # str() para cambiar un dato a tipo string
 
-print()
+print("\n-----------------------------------------------------------------------------------------------------------\n")
 
 # Variable tipo Float
+print("\t Variable Tipo Float \t\n")
 dinero = 155.89
 
-print(type(dinero))
-print("Tengo en mi cuenta: " + str(dinero) + " pesos. Estoy quebrado Unu")
+print("La variable dinero es del tipo: " + str(type(dinero)))
+
+print("\nTengo en mi cuenta: " + str(dinero) + " pesos. Estoy quebrado Unu")
+
+print("\n-----------------------------------------------------------------------------------------------------------\n")
+
+# Variable tipo booleano
+print("\t Variable Tipo Booleano \t\n")
+
+print("True es un valor tipo: " + str(type(True)))
+print("False es un valor tipo: " + str(type(False)))
+
+print("\n¿Es 1 igual a 1?: " + str(1 == 1))
+
+print("¿Es 5 igual a 2?: " + str(5 == 2))
 
 
 """
