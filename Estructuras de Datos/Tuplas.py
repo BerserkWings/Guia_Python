a4cd5a5d4ab79276b993d78a59978204b132ff04
BerserkWings <manchas_0207@hotmail.com>
2024-02-15 12:34:43 -0600

    Añadiendo Carpeta Estructuras de Datos

---
+++ Estructuras de Datos/Tuplas.py
@@ -0,0 +1,7 @@
+attacks = ['Phishing', 'DDoS', 'SQL Injection', 'Man In The Middle', 'Cross-Site Scripting']
+
+# Convertir cada elemento a mayúsculas usando una comprensión de lista
+attacks_uppercase = [attack.upper() for attack in attacks]
+
+# Imprimir la lista resultante
+print(attacks_uppercase)
